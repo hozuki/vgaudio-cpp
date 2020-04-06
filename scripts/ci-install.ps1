982ba1ecc187f37f99ab93cfda813d5734172388
George Wu <uiharu@buaa.edu.cn>
2020-04-06 16:48:13 +0200

    Initial Linux (Ubuntu) CI

--- scripts/ci-install.ps1
+++ scripts/ci-install.ps1
@@ -2,3 +2,8 @@ git submodule update --init --recursive
 cd ext/zlib
 git checkout v1.2.11
 cd ../..
+
+if ($isLinux)
+{
+    sudo apt install zlib1g-dev -y
+}
--- scripts/dep/build-linux.ps1
+++ scripts/dep/build-linux.ps1
@@ -1,1 +1,6 @@
-echo 'TODO'
+mkdir build -ErrorAction SilentlyContinue
+cd build
+& cmake -G "UNIX" -D CMAKE_BUILD_TYPE=Release -D CMAKE_WORKING_DIR="$pwd" ..
+make
+
+cd ..
